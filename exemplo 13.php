dd3b2156d5f39c21a7ec332812d48251ef46990a
AnaCaroline11 <carolinelucio08@gmail.com>
2017-05-06 18:20:32 -0300

    Create exemplo 13.php

---
+++ exemplo 13.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<meta charset ="utf-8"/>
+</head>
+<body>
+<h1> Exemplo13 </h1>
+<?php
+function soma ($n1,$n2){
+  return $n1+$n2;
+}
+function imprimir(){
+  return " O RESULTADO DA SOMA É:";
+}
+?>
+</body>
+</html>
